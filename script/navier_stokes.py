a544eec496b936935edd3a93988dee43284ded87
Konstantin Belotelov <33226823+CostiaB@users.noreply.github.com>
2022-11-22 21:25:03 +0500

    Add files via upload

--- script/navier_stokes.py
+++ script/navier_stokes.py
@@ -1,66 +1,94 @@
 from tqdm import tqdm
 import numpy as np
+import torch
 import os
 from script.pressure_poisson_periodic import pressure_poisson_periodic, build_up_b
 from script.channel_shape import channel_shape
+from copy import copy
+from collections import deque
 
 def save_file(file, name, save_folder):
     np.save(os.path.join(save_folder, name), file)
 
 def navier_stokes(u, v, freq,
                   w, h, d, s,
-                  bottom_left, top_left, bottom_right,
+                  bottom_left, top_left,
+                  bottom_right, top_right,
                   anode_value, cathode_value,
                   border_value,
                   ny, nx,
                   p, b,
                   rho, nu, 
                   dt, dx, dy,
-                  F, P0, nt, n_save=10, p_it=1500, save_folder='', stepcount=1,
+                  F, P0, nt, max_vec_len,
+                  n_save=10, p_it=1500, save_folder='', stepcount=1,
                   save_vectors=True,
                   silent=False):
     
     
     if save_vectors:
-        u_gr = []
-        v_gr = []
-        p_gr = []
-        ch_it = []
+        print(max_vec_len)
+        u_gr = deque(maxlen=max_vec_len)
+        v_gr = deque(maxlen=max_vec_len)
+        p_gr = deque(maxlen=max_vec_len)
+        ch_it = deque(maxlen=max_vec_len)
+        ts = deque([0], maxlen=max_vec_len)
 
    
     
     #check if p initialization is working
-    '''    
-        b = build_up_b(rho, dt, dx, dy, u, v, w, h, d, s, 
-                       bottom_left, bottom_right, top_left, 
+        
+    b = build_up_b(rho, dt, dx, dy, u, v, w, h, d, s, 
+                       bottom_left, bottom_right,
+                       top_left,  top_right,
                        anode_value, cathode_value, border_value,
                        nx, ny)
-        
-        p = pressure_poisson_periodic(p,
-                                  freq,
-                                  w, h, d, s,
-                                  bottom_left, top_left, bottom_right,
-                                  anode_value, cathode_value,
-                                  border_value,
-                                  ny, nx,
-                                   b,
-                                  rho, nu, 
-                                  dx, dy,
-                                  P0, stepcount,
-                                  1, dt)'''
+    p = pressure_poisson_periodic(p,
+                              freq,
+                              w, h, d, s,
+                              bottom_left, top_left,
+                              bottom_right, top_right,
+                              anode_value, cathode_value,
+                              border_value,
+                              ny, nx,
+                               b,
+                              rho, nu, 
+                              dx, dy,
+                              P0, stepcount,
+                              p_it, dt)
     
-
+    
+    freq_step = 0
+    J = copy(F)
+    
     for t in tqdm(range(nt)):
-        un = u.copy()
-        vn = v.copy()
+        
+        
+        #v[315:326, 315:326] = 0 #test
+        #u[315:326, 315:326] = 0 #test
+        
+ 
+        if t % 10 == 0:
+            time_mult = 1/(100 * freq)
+            freq_cos = np.cos(2 * np.pi * (freq_step * time_mult) * freq)  
+            F = J * freq_cos
+            freq_step += 1
+            
+        
+        
+        un = u.clone()
+        vn = v.clone()
+        
         b = build_up_b(rho, dt, dx, dy, u, v, w, h, d, s, 
-                       bottom_left, bottom_right, top_left, 
+                       bottom_left, bottom_right,
+                       top_left,  top_right,
                        anode_value, cathode_value, border_value,
                        nx, ny)
         p = pressure_poisson_periodic(p,
                               freq,
                               w, h, d, s,
-                              bottom_left, top_left, bottom_right,
+                              bottom_left, top_left,
+                              bottom_right, top_right,
                               anode_value, cathode_value,
                               border_value,
                               ny, nx,
@@ -82,7 +110,7 @@ def navier_stokes(u, v, freq,
                         nu * (dt / dx**2 * 
                         (un[1:-1, 2:] - 2 * un[1:-1, 1:-1] + un[1:-1, 0:-2]) +
                         dt / dy**2 * 
-                        (un[2:, 1:-1] - 2 * un[1:-1, 1:-1] + un[0:-2, 1:-1])))
+                        (un[2:, 1:-1] - 2 * un[1:-1, 1:-1] + un[0:-2, 1:-1])) + F * dt)
 
 
         v[1:-1, 1:-1] = (vn[1:-1, 1:-1] -
@@ -96,27 +124,84 @@ def navier_stokes(u, v, freq,
                         dt / dy**2 * 
                         (vn[2:, 1:-1] - 2 * vn[1:-1, 1:-1] + vn[0:-2, 1:-1])) +
                         0*F * dt)
+        
+                # Periodic BC u @ x = 2     
+        u[1:-1, -1] = (un[1:-1, -1] - un[1:-1, -1] * dt / dx * 
+                      (un[1:-1, -1] - un[1:-1, -2]) -
+                       vn[1:-1, -1] * dt / dy * 
+                      (un[1:-1, -1] - un[0:-2, -1]) -
+                       dt / (2 * rho * dx) *
+                      (p[1:-1, 0] - p[1:-1, -2]) + 
+                       nu * (dt / dx**2 * 
+                      (un[1:-1, 0] - 2 * un[1:-1,-1] + un[1:-1, -2]) +
+                       dt / dy**2 * 
+                      (un[2:, -1] - 2 * un[1:-1, -1] + un[0:-2, -1])) + F * dt)
+    
+        # Periodic BC u @ x = 0
+        u[1:-1, 0] = (un[1:-1, 0] - un[1:-1, 0] * dt / dx *
+                     (un[1:-1, 0] - un[1:-1, -1]) -
+                      vn[1:-1, 0] * dt / dy * 
+                     (un[1:-1, 0] - un[0:-2, 0]) - 
+                      dt / (2 * rho * dx) * 
+                     (p[1:-1, 1] - p[1:-1, -1]) + 
+                      nu * (dt / dx**2 * 
+                     (un[1:-1, 1] - 2 * un[1:-1, 0] + un[1:-1, -1]) +
+                      dt / dy**2 *
+                     (un[2:, 0] - 2 * un[1:-1, 0] + un[0:-2, 0])) + F * dt)
+    
+        # Periodic BC v @ x = 2
+        v[1:-1, -1] = (vn[1:-1, -1] - un[1:-1, -1] * dt / dx *
+                      (vn[1:-1, -1] - vn[1:-1, -2]) - 
+                       vn[1:-1, -1] * dt / dy *
+                      (vn[1:-1, -1] - vn[0:-2, -1]) -
+                       dt / (2 * rho * dy) * 
+                      (p[2:, -1] - p[0:-2, -1]) +
+                       nu * (dt / dx**2 *
+                      (vn[1:-1, 0] - 2 * vn[1:-1, -1] + vn[1:-1, -2]) +
+                       dt / dy**2 *
+                      (vn[2:, -1] - 2 * vn[1:-1, -1] + vn[0:-2, -1])))
+    
+        # Periodic BC v @ x = 0
+        v[1:-1, 0] = (vn[1:-1, 0] - un[1:-1, 0] * dt / dx *
+                     (vn[1:-1, 0] - vn[1:-1, -1]) -
+                      vn[1:-1, 0] * dt / dy *
+                     (vn[1:-1, 0] - vn[0:-2, 0]) -
+                      dt / (2 * rho * dy) * 
+                     (p[2:, 0] - p[0:-2, 0]) +
+                      nu * (dt / dx**2 * 
+                     (vn[1:-1, 1] - 2 * vn[1:-1, 0] + vn[1:-1, -1]) +
+                      dt / dy**2 * 
+                     (vn[2:, 0] - 2 * vn[1:-1, 0] + vn[0:-2, 0])))
 
 
       # Wall  and electrodes BC: u,v = 0 
         v = channel_shape(field=v,
                   w=w, h=h, d=d, s=s,
                   bottom_left=bottom_left, top_left=top_left,
-                  bottom_right=bottom_right,
+                  bottom_right=bottom_right, top_right=top_right,
                   anode_value=anode_value, cathode_value=cathode_value,
                   border_value=border_value, nx=nx,
                   ny=ny)
         u = channel_shape(field=u,
                   w=w, h=h, d=d, s=s,
                   bottom_left=bottom_left, top_left=top_left,
-                  bottom_right=bottom_right,
+                  bottom_right=bottom_right, top_right=top_right,
                   anode_value=anode_value, cathode_value=cathode_value,
                   border_value=border_value, nx=nx,
                   ny=ny)
-        #v[-1, top_left[1]:] = 0 #F*np.cos(2*np.pi*(stepcount*dt)*freq)  
-        #v[0, bottom_left[1]:bottom_right[1]] = 0 # -F*np.cos(2*np.pi*(stepcount*dt)*freq) 
         
-        if p.max() > 5:
+        
+        
+        v[-1, top_left[1]:] = 0 #F*np.cos(2*np.pi*(stepcount*dt)*freq)  
+        v[0, bottom_left[1]:bottom_right[1]] = 0 # -F*np.cos(2*np.pi*(stepcount*dt)*freq) 
+        
+        u[-1, top_left[1]:] = 0 #F*np.cos(2*np.pi*(stepcount*dt)*freq)  
+        u[0, bottom_left[1]:bottom_right[1]] = 0 # -F*np.cos(2*np.pi*(stepcount*d
+        '''
+        v[315:326, 315:326] = 0 #test
+        u[315:326, 315:326] = 0 #test
+        '''
+        if any([p.max(), u.max(), v.max()]) > 1:
             if not silent:
                 print(f'\nit {t}, b_max : {b.max():.2E}, p_max : {p.max():.2E}, u_max : {u.max():.2E}, v_max : {v.max():.2E}')
             print('Value is too big! Calculation was stopped! ')
@@ -126,18 +211,32 @@ def navier_stokes(u, v, freq,
             
         #v[0, bottom_left[1]:bottom_right[1]] = F*np.cos(2*np.pi*(stepcount*dt)*freq)
         #v[-1, top_left[1]:-1] = -F*np.cos(2*np.pi*(stepcount*dt)*freq) 
+        
+        
+
+    
+        
         if t % n_save == 0:
             if not silent:
                 print(f'\nit {t}, b_max : {b.max():.2E}, p_max : {p.max():.2E}, u_max : {u.max():.2E}, v_max : {v.max():.2E}')
-            if save_vectors:    
-                u_gr.append(u.copy())
-                v_gr.append(v.copy())
-                p_gr.append(p.copy())
+                
+                w = freq/(2*np.pi)
+                a = (w/(2*nu))**(1/2)*(1 + 1j)
+                eta = nu * rho 
+                
+                f_17_5 = (450**2/(12*eta) *  (J/1500)/dx)/1e18
+                print(f'u_max: {f_17_5 :.2E}, u: {u[100:550, 600].mean():.2E}')
+            if save_vectors:  
+                u_gr.append(u.clone().cpu())
+                v_gr.append(v.clone().cpu())
+                p_gr.append(p.clone().cpu())
                 ch_it.append(t)
+                ts.append(freq_step * time_mult)
             
         stepcount += 1
-      
-        #udiff = (numpy.sum(u) - numpy.sum(un)) / numpy.sum(u)
+        if stepcount % 500 == 0:
+            udiff = (torch.sum(u) - torch.sum(un)) / torch.sum(u)
+            print(udiff)
         
         
     '''    
@@ -158,13 +257,15 @@ def navier_stokes(u, v, freq,
                   ny=ny)'''
     if not silent:
         print('Calculation complete')
+    
         
     if save_vectors:
-        files = [u_gr, v_gr, p_gr, ch_it]
-        names = ['u_gr', 'v_gr', 'p_gr', 'ch_it']
+        files = [u_gr, v_gr, p_gr, ch_it, ts]
+        names = ['u_gr', 'v_gr', 'p_gr', 'ch_it', 'ts']
         
         for file, name in zip(files, names):
             save_file(file, name, save_folder)
     
         print('Calculated data was saved')
+        
     return u, v, p
---
+++ script/untitled1.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+u = np.load('/home/lapa/flow_modeling_1/results/navier_2022_11_08_11_10/u_gr.npy')
+ch_it = np.load('/home/lapa/flow_modeling_1/results/navier_2022_11_08_11_10/ch_it.npy')
+ts = np.load('/home/lapa/flow_modeling_1/results/navier_2022_11_08_11_10/ts.npy')
+
+dt = 1e-9
+nt = 1000
+w = 1/(2*np.pi)
+w = 1
+t = nt / 200
+nu = 1.25e-6 * 1e12
+
+rho = 1.5354e3 / 1e18
+eta = nu * rho 
+dx = 0.05
+F = 0.0015 
+
+
+a = -(F/1500)/dx * 1/rho * np.exp(1j*w*t)
+a = F/6.3e6
+
+u_max = 1j*a/w * np.exp(-1j*w*t) * dt
+
+
+tmp = [1j*a/w * np.exp(-1j*w*t*6.5) for t in ts]
+plt.plot(tmp)
+plt.ylim(min(tmp), max(tmp))
+print([(1j*a/w * np.exp(-1j*w*t) ) for t in ch_it])
+u_m = a * np.exp(-1j*w*t) *(450**2) / (12  * nu)
+
+print(np.abs(u_max))
+f_17_5 = (450**2/(12*eta) *  (F/1500)/dx) / 1e18
+
+print(f_17_5)
+
+print(f'u real {u[-2][650//2, 1400//2]}, u_max {np.abs(u_max)}, u_m {np.abs(u_m)}')
+
+u_vec = [uu[100:550, 600].mean() for uu in u]
+
+formula = r'$\displaystyle \bar{v} = -\frac{h^{2}}{12\eta} * \frac{dp}{dx}$'
+
+plt.rcParams['text.usetex'] = True
+plt.rcParams.update({'font.size': 22})
+plt.figure(figsize=(15, 10), dpi=250)
+
+plt.hlines(f_17_5, 0, len(u), color='r', linestyles='dashed')
+plt.text(len(u)*0.95, f_17_5*0.8, formula , fontsize=20, 
+         bbox=dict(facecolor='red', alpha=0.25))
+plt.plot(u_vec)
+plt.plot(tmp)
+plt.ylim(min(u_vec), max(u_vec))
+plt.xlabel('Iterations', fontsize=20)
+plt.ylabel(r'Velocity ($\displaystyle \frac{m}{s}$)', fontsize=20)
