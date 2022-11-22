88bc2b0583e31c62fda7eac5e25026469a9d84bd
Konstantin Belotelov <33226823+CostiaB@users.noreply.github.com>
2022-11-22 21:24:12 +0500

    Add files via upload

--- calc_navier.py
+++ calc_navier.py
@@ -1,5 +1,6 @@
 def main(nt, p_it, freq):
     import numpy as np
+    import torch
     import gc
     import os
     import time
@@ -39,6 +40,7 @@ def main(nt, p_it, freq):
     bottom_left = params_channel_shape.bottom_left
     top_left = params_channel_shape.top_left
     bottom_right = params_channel_shape.bottom_right
+    top_right = params_channel_shape.top_right
 
     v = params_channel_shape.v
     u = params_channel_shape.u
@@ -49,8 +51,8 @@ def main(nt, p_it, freq):
     
 
     try:
-        
-        n_save = nt // 200 if nt > 200 else 1
+        max_vec_len = 260
+        n_save = nt // max_vec_len if nt > max_vec_len else 1
         save_folder = './results/'
         save_folder += 'navier_'
         save_folder += time.strftime('%Y_%m_%d_%H_%M', time.localtime())
@@ -59,40 +61,49 @@ def main(nt, p_it, freq):
             
         u, v, p = navier_stokes(u, v, freq,
                           w, h, d, s,
-                          bottom_left, top_left, bottom_right,
+                          bottom_left, top_left,
+                          bottom_right, top_right,
                           anode_value, cathode_value,
                           border_value,
                           ny, nx,
                           p, b,
                           rho, nu, 
                           dt, dx, dy,
-                          F, P0, nt, n_save, p_it, save_folder=save_folder)
+                          F, P0, nt, max_vec_len, n_save,
+                          p_it, save_folder=save_folder, save_vectors=True)
         
         p_gif = channel_shape(field = field,
                           w=w, h=h, d=d, s=s,
                           bottom_left=bottom_left, top_left=top_left,
-                          bottom_right=bottom_right,
+                          bottom_right=bottom_right, top_right=top_right,
                           anode_value=anode_value+10000, cathode_value=cathode_value+10000,
                           border_value=border_value+10000, nx=nx,
-                          ny=ny)
-        
-        np.save(save_folder + '/' + 'channel_for_gif', p)
+                          ny=ny).to('cpu').numpy()
+        np.save(save_folder + '/' + 'channel_for_gif', p.to('cpu'))
         
         names = ['p_init_0', 'u_init_0', 'v_init_0']
         files = ['p_gr.npy', 'u_gr.npy', 'v_gr.npy']
+
+        names = ['u_init_0']
+        files = ['u_gr.npy']
+
         ch_it = np.load(save_folder + '/ch_it.npy').tolist()
+        
         
         for file, name in zip(files, names):
             gc.collect()
-            imgs = np.load(save_folder + '/' + file)
+            imgs = np.load(save_folder + '/' + file, allow_pickle=True)
+            imgs = np.array([i.numpy() for i in imgs])
             vmin = 0 if len(imgs[imgs<0]) == 0 else np.mean(imgs[imgs<0])
             vmax = 0 if len(imgs[imgs>0]) == 0 else np.mean(imgs[imgs>0])
+        
             if 0 not in [vmin, vmax]:    
                 mean = np.mean(np.abs([vmin, vmax]))
                 vmin, vmax = -mean, mean
                 
             if vmax > 0.2:
                 vmin, vmax = -0.02, 0.02
+            #vmin, vmax = 1e-19, -1e-19
             plot_gif(imgs, p_gif, name, vmin, vmax, ch_it, save_folder)
             gc.collect()
             print(f'Gif {name} was saved')
@@ -102,6 +113,33 @@ def main(nt, p_it, freq):
         write_params(save_folder, p_it=p_it, nt=nt, freq=freq)
         print('Params was saved')
         print(f'Directory name: {save_folder}')
+        
+        
+        import matplotlib.pyplot as plt
+        x = np.linspace(0, 2, nx)
+        y = np.linspace(0, 2, ny)
+        X, Y = np.meshgrid(x, y)
+        
+        u = u.to('cpu')
+        v = v.to('cpu')
+        
+
+        fig = plt.figure(figsize = (20,15), dpi=400)
+        st = 15
+        plt.quiver(X[::st, ::st], Y[::st, ::st], u[::st, ::st],
+                   v[::st, ::st], width=1e-3)
+        plt.savefig(save_folder + '/final_speed.png')
+        
+        fig = plt.figure(figsize = (20,15), dpi=400)
+        st = 1
+        plt.quiver(X[200:400:st, 200:400:st], Y[200:400:st, 200:400:st],
+                   u[200:400:st, 200:400:st], v[200:400:st, 200:400:st], width=1e-3)
+        plt.savefig(save_folder + '/final_speed_center.png')
+        
+        
+        fig = plt.figure(figsize = (20,15))
+        plt.plot(u[:, 500])
+        plt.savefig(save_folder + '/u_slice.png')
         
         
         
@@ -114,8 +152,8 @@ if __name__ == '__main__':
     
     import argparse
     
-    nt = 20
-    p_it = 1500
+    nt = 20000
+    p_it = 50
     freq = .1
     
     parser = argparse.ArgumentParser(description='Create params for calculation')
--- params_channel_shape.py
+++ params_channel_shape.py
@@ -1,23 +1,27 @@
-import numpy as np
+import torch
 
 
 scale = 1
 nx = int((400+300+400+300)/scale)
 ny = int((250+150+250)/scale)
 
-field = np.ones((ny, nx))
+field = torch.ones((ny, nx), device='cuda', requires_grad=False, dtype=torch.double)
 anode_value = 0
 cathode_value = 0
 border_value = 0
 w = int(40/scale)
-h = int(20/scale)
+h = 20
 d = int(150/scale)
 s = int(50/scale)
-bottom_left = (int(650/scale), int(10/scale))
-top_left = (int(250/scale), int(1140/scale))
-bottom_right = (int(250/scale), int(260/scale))
+bottom_left = (int(100/scale), int(400/scale))
+bottom_right = (int(100/scale), int(300/scale)) #diff from point (0;ny)
+top_left = (int(100/scale), int(400/scale)) #form point (0;0)
+top_right = (int(100/scale), int(300/scale)) #diff from point (0;ny)
 
-v = np.zeros((ny, nx)) / 100
-u = np.zeros((ny, nx)) / 100
-p = np.zeros((ny, nx)) / 100000
-b = np.zeros((ny, nx)) / 100000
+
+
+
+v = torch.zeros((ny, nx), device='cuda', requires_grad=False, dtype=torch.double)
+u = torch.zeros((ny, nx), device='cuda', requires_grad=False, dtype=torch.double)
+p = torch.zeros((ny, nx), device='cuda', requires_grad=False, dtype=torch.double)
+b = torch.zeros((ny, nx), device='cuda', requires_grad=False, dtype=torch.double)
